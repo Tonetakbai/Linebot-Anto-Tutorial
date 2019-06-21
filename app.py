9216b18163fc7f0a0baf144329d904e718471f38
Tonetakbai <50025569+Tonetakbai@users.noreply.github.com>
2019-06-22 04:09:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,17 +10,17 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage
 )
 
-line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
-handler = WebhookHandler('YOUR_CHANNEL_SECRET')
+line_bot_api = LineBotApi('jJ89Z8MyAFwIzkrCVxSe4tqdY/FR7lsSiXNg6f++MUXLM+XOosrZ3a5VLemXT4G+30svsp18hmh4N1dIQFArrM/1wecLkpll2ZN/Qgk2MFOANhz4AKh220GlgZ5d5Z4em/LdIEgqFIZ6Ppuq+OokWAdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('0fbe52461d6652bf45b5ba23f1c33404')
 
 app = Flask(__name__)
 
 # username of anto.io account
-user = 'YOUR_USERNAME'
+user = 'tonetakbai'
 # key of permission, generated on control panel anto.io
-key = 'YOUR_KEY'
+key = 'kT8rzk920j3nqfEnLbX8rC4JahyobWUguj6pVxb8'
 # your default thing.
-thing = 'YOUR_THING'
+thing = 'myDevice'
 
 anto = antolib.Anto(user, key, thing)
 
@@ -43,12 +43,29 @@ def callback():
     return 'OK'
 
 
-@handler.add(MessageEvent, message=TextMessage)
+@@handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = event.message.text
-    # line_bot_api.reply_message(
-    #     event.reply_token,
-    #     TextSendMessage(text="Turn Off channel1"))
+    if(message == 'channel1 on'):
+        anto.pub('myChannel1', 1)
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text="Turn On channel1"))
+    elif(message == 'channel1 off'):
+        anto.pub('myChannel1', 0)
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text="Turn Off channel1"))
+    elif(message == 'channel2 on'):
+        anto.pub('myChannel2', 1)
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text="Turn On channel2"))
+    elif(message == 'channel2 off'):
+        anto.pub('myChannel2', 0)
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text="Turn Off channel2"))
 
 if __name__ == "__main__":
     anto.mqtt.connect()
